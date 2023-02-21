3b592a345e542202bf03e79381dd396743b6a021
KevanLam <90704410+KevanLam@users.noreply.github.com>
2023-02-21 18:20:39 +0100

    Math 2 clamp + asinh

---
+++ src/ch/epfl/javions/Math2.java
@@ -0,0 +1,15 @@
+package ch.epfl.javions;
+
+public final class Math2 {
+    private Math2() {}
+
+    public static int clamp(int min, int v, int max) throws IllegalArgumentException{
+        if(min>max) throw new IllegalArgumentException();
+        if(v<min) return min;
+        else if (v>max) return max;
+        else return v;
+    }
+    public static double asinh(double x){
+        return Math.log((x+Math.sqrt(1+Math.pow(x,2))));
+    }
+}
