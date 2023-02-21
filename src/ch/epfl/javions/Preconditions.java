645e68dbca8a5a84f8cecb41ed8bb92cfa50b428
KevanLam <90704410+KevanLam@users.noreply.github.com>
2023-02-21 18:16:55 +0100

    preconditions static + preconditions done

--- src/ch/epfl/javions/Preconditions.java
+++ src/ch/epfl/javions/Preconditions.java
@@ -1,8 +1,8 @@
 package ch.epfl.javions;
 
 public final class Preconditions {
-    private Preconditions(){};
-    void checkArgument(boolean shouldBeTrue) throws IllegalArgumentException{
+    private Preconditions(){}
+    static void checkArgument(boolean shouldBeTrue) throws IllegalArgumentException{
             if(!shouldBeTrue) throw new IllegalArgumentException();
     }
 }
