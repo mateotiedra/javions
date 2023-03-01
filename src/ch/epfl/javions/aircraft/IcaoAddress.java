9ead75865e866d575bde3bc6d0064b769221e326
KevanLam <90704410+KevanLam@users.noreply.github.com>
2023-03-01 14:16:15 +0100

    IcaoAddress record done fr

--- src/ch/epfl/javions/aircraft/IcaoAddress.java
+++ src/ch/epfl/javions/aircraft/IcaoAddress.java
@@ -2,8 +2,11 @@ package ch.epfl.javions.aircraft;
 
 import ch.epfl.javions.Preconditions;
 
+import java.util.regex.Pattern;
+
 public record IcaoAddress(String string){
+    private static final Pattern patternICAO = Pattern.compile("[0-9A-F]{6}");
     public IcaoAddress {
-        Preconditions.checkArgument(string.length() == 6);
+        Preconditions.checkArgument(patternICAO.matcher(string).matches());
     }
 }
