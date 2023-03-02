1ea7a9fd45aa7fc47179ecf7699a560a472f6c14
KevanLam <90704410+KevanLam@users.noreply.github.com>
2023-03-02 19:22:59 +0100

    aircraftdata

--- src/ch/epfl/javions/aircraft/AircraftData.java
+++ src/ch/epfl/javions/aircraft/AircraftData.java
@@ -1,5 +1,13 @@
 package ch.epfl.javions.aircraft;
 
-public record AircraftData(AircraftDescription aircraftDescription, IcaoAddress icaoAddress, AircraftRegistration aircraftRegistration, WakeTurbulenceCategory wakeTurbulenceCategory) {
+import java.util.Objects;
 
+public record AircraftData(AircraftDescription aircraftDescription, IcaoAddress icaoAddress,String model, AircraftRegistration aircraftRegistration, WakeTurbulenceCategory wakeTurbulenceCategory) {
+    public AircraftData{
+        Objects.requireNonNull(aircraftDescription);
+        Objects.requireNonNull(icaoAddress);
+        Objects.requireNonNull(model);
+        Objects.requireNonNull(aircraftRegistration);
+        Objects.requireNonNull(wakeTurbulenceCategory);
+    }
 }
