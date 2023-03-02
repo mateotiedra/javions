e452719f3b204f4fb127d517b382c74acb2cee0c
mateotiedra <mateo@tiedra.com>
2023-03-02 19:10:57 +0100

    add submit tokens

--- src/ch/epfl/cs108/Submit.java
+++ src/ch/epfl/cs108/Submit.java
@@ -32,9 +32,9 @@ public final class Submit {
     // CONFIGURATION
     // -------------
     // Jeton du premier membre du groupe
-    private static final String TOKEN_1 = "";
+    private static final String TOKEN_1 = "Shen4ahg";
     // Jeton du second membre (identique au premier pour les personnes travaillant seules)
-    private static final String TOKEN_2 = "";
+    private static final String TOKEN_2 = "Kie7aich";
     // -------------
 
     private static final String ZIP_ENTRY_NAME_PREFIX = "Javions/";
