86223bd1058c94a68c190a64a244dd480578e8d4
mateotiedra <mateo@tiedra.com>
2023-03-03 13:27:36 +0100

    rename atributs for sigcheck

--- src/ch/epfl/javions/aircraft/AircraftData.java
+++ src/ch/epfl/javions/aircraft/AircraftData.java
@@ -1,18 +1,21 @@
 package ch.epfl.javions.aircraft;
 
 import java.util.Objects;
+
 /**
  * This class represents the data of an aircraft.
+ *
  * @author Kevan Lam (356395)
  * @author Mateo Tiedra (356525)
- *
  */
-public record AircraftData(AircraftRegistration aircraftRegistration, AircraftTypeDesignator typeDesignator,String model, AircraftDescription aircraftDescription, WakeTurbulenceCategory wakeTurbulenceCategory) {
-    public AircraftData{
-        Objects.requireNonNull(aircraftDescription);
+public record AircraftData(AircraftRegistration registration, AircraftTypeDesignator typeDesignator,
+                           String model, AircraftDescription description,
+                           WakeTurbulenceCategory wakeTurbulenceCategory) {
+    public AircraftData {
+        Objects.requireNonNull(description);
         Objects.requireNonNull(typeDesignator);
         Objects.requireNonNull(model);
-        Objects.requireNonNull(aircraftRegistration);
+        Objects.requireNonNull(registration);
         Objects.requireNonNull(wakeTurbulenceCategory);
     }
 }
