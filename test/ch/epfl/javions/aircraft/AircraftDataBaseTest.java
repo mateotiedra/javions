76400aa9c4d5a32436b166628599d2762f913559
KevanLam <90704410+KevanLam@users.noreply.github.com>
2023-03-06 12:35:46 +0100

    Ed de merde

--- test/ch/epfl/javions/aircraft/AircraftDataBaseTest.java
+++ test/ch/epfl/javions/aircraft/AircraftDataBaseTest.java
@@ -3,13 +3,80 @@ package ch.epfl.javions.aircraft;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.net.URLDecoder;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.junit.jupiter.api.Assertions.*;
 /**
  * This class represents the AircraftDataBaseTest
  * @author Kevan Lam (356395)
  */
 public class AircraftDataBaseTest {
+    private AircraftDatabase getDatabase() {
+        // Try to get the database from the resources
+        var aircraftResourceUrl = getClass().getResource("/aircraft.zip");
+        if (aircraftResourceUrl != null)
+            return new AircraftDatabase(URLDecoder.decode(aircraftResourceUrl.getFile(), UTF_8));
+
+        // Try to get the database from the JAVIONS_AIRCRAFT_DATABASE environment variable
+        // (only meant to simplify testing of several projects with a single database)
+        var aircraftFileName = System.getenv("JAVIONS_AIRCRAFT_DATABASE");
+        if (aircraftFileName != null)
+            return new AircraftDatabase(aircraftFileName);
+
+        throw new Error("Could not find aircraft database");
+    }
+    @Test
+    void aircraftDatabaseGetWorksWithLastLineOfFile() throws IOException {
+        var aircraftDatabase = getDatabase();
+        var aircraftData = aircraftDatabase.get(new IcaoAddress("E808C0"));
+        assertNotNull(aircraftData);
+        assertEquals(new AircraftRegistration("CC-DAW"), aircraftData.registration());
+    }
+
+    @Test
+    void aircraftDatabaseGetWorksWithAddressGreaterThanLastOneOfFile() throws IOException {
+        var aircraftDatabase = getDatabase();
+        var aircraftData = aircraftDatabase.get(new IcaoAddress("FFFF01"));
+        assertNull(aircraftData);
+    }
+
+    @Test
+    void aircraftDatabaseGetReturnsCorrectData() throws IOException {
+        var aircraftDatabase = getDatabase();
+        var aircraftData = aircraftDatabase.get(new IcaoAddress("4B1805"));
+        assertNotNull(aircraftData);
+        assertEquals(new AircraftRegistration("HB-JCN"), aircraftData.registration());
+        assertEquals(new AircraftTypeDesignator("BCS3"), aircraftData.typeDesignator());
+        assertEquals("AIRBUS A220-300", aircraftData.model());
+        assertEquals(new AircraftDescription("L2J"), aircraftData.description());
+        assertEquals(WakeTurbulenceCategory.MEDIUM, aircraftData.wakeTurbulenceCategory());
+    }
+
+    @Test
+    void aircraftDatabaseGetWorksWithEmptyColumns() throws IOException {
+        var aircraftDatabase = getDatabase();
+        var aircraftData = aircraftDatabase.get(new IcaoAddress("AAAAAA"));
+        assertNotNull(aircraftData);
+        assertEquals(new AircraftRegistration("N787BK"), aircraftData.registration());
+        assertEquals(new AircraftTypeDesignator(""), aircraftData.typeDesignator());
+        assertEquals("", aircraftData.model());
+        assertEquals(new AircraftDescription(""), aircraftData.description());
+        assertEquals(WakeTurbulenceCategory.UNKNOWN, aircraftData.wakeTurbulenceCategory());
+    }
+    @Test
+    void aircraftDatabaseGetReturnsNullWhenAddressDoesNotExist() throws IOException {
+        var aircraftDatabase = getDatabase();
+        assertNull(aircraftDatabase.get(new IcaoAddress("123456")));
+    }
+    @Test
+    void aircraftDatabaseGetWorksWithFirstLineOfFile() throws IOException {
+        var aircraftDatabase = getDatabase();
+        var aircraftData = aircraftDatabase.get(new IcaoAddress("0086AB"));
+        assertNotNull(aircraftData);
+        assertEquals(new AircraftRegistration("ZS-CNA"), aircraftData.registration());
+    }
+
     @Test
     void aircraftConstructorSuccess(){
         assertDoesNotThrow(() -> new AircraftDatabase("/aircraft.zip"));
@@ -17,27 +84,5 @@ public class AircraftDataBaseTest {
     @Test
     void aircraftConstructorFails(){
         assertThrows(NullPointerException.class, () -> new AircraftDatabase(null));
-    }
-    //Test with data that exist
-    @Test
-    void aircraftDataBaseWorksWithCorrectString() throws IOException {
-        AircraftDatabase database = new AircraftDatabase("/aircraft.zip");
-        IcaoAddress address1 = new IcaoAddress("E941FF");
-        AircraftData expected1 = new AircraftData(new AircraftRegistration("CP-3080"), new AircraftTypeDesignator("C208"), "CESSNA 208 Caravan", new AircraftDescription("L1T"), WakeTurbulenceCategory.LIGHT);
-        AircraftData actual1 = database.get(address1);
-        IcaoAddress address3 = new IcaoAddress("440A1E");
-        AircraftData expected3 = new AircraftData(new AircraftRegistration("OE-IDJ"), new AircraftTypeDesignator("A320"), "AIRBUS A-320", new AircraftDescription("L2J"), WakeTurbulenceCategory.MEDIUM);
-        AircraftData actual3 = database.get(address3);
-        assertEquals(expected3, actual3);
-        assertEquals(expected1, actual1);
-    }
-    // Test that does not work cause the data does not exist
-    @Test
-    void aircraftDataBaseDoesNotWorksWithInexistantIcao() throws IOException{
-        AircraftDatabase database = new AircraftDatabase("/aircraft.zip");
-        IcaoAddress address2 = new IcaoAddress("B00001");
-        AircraftData expected2 = null;
-        AircraftData actual2 = database.get(address2);
-        assertEquals(expected2, actual2);
     }
 }
