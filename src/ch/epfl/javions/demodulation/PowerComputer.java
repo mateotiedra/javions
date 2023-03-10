49fed559e8e8e89725245e493f2ddf5d8cc9cb60
mateotiedra <mateo@tiedra.com>
2023-03-10 14:55:10 +0100

    add powercomputer and test

--- src/ch/epfl/javions/demodulation/PowerComputer.java
+++ src/ch/epfl/javions/demodulation/PowerComputer.java
@@ -5,21 +5,63 @@ import ch.epfl.javions.Preconditions;
 import java.io.IOException;
 import java.io.InputStream;
 
+/**
+ * This class is used to compute the power of the radio sample.
+ * <p>
+ * author: Mateo Tiedra (356525)
+ */
 public final class PowerComputer {
-    private int[] batch;
-    private int batchsize;
-    private SamplesDecoder samplesDecoder;
-    private InputStream inputStream;
-    public PowerComputer(InputStream stream, int batchSize){
-        Preconditions.checkArgument(batchSize > 0 && batchSize % 8 == 0);
-        this.batchsize = batchSize;
-        this.batch = new int[batchSize];
-        this.samplesDecoder = new SamplesDecoder(stream, 2*batchSize);
+    private final short[] samples;
+    private final int batchSize;
+    private final SamplesDecoder samplesDecoder;
 
+    private final short[] lastHeightSamples = new short[8];
+
+    /**
+     * Constructor of the PowerComputer class.
+     *
+     * @param stream    the stream of the samples
+     * @param batchSize the size of the batch
+     */
+    public PowerComputer(InputStream stream, int batchSize) {
+        Preconditions.checkArgument(batchSize > 0 && batchSize % 8 == 0);
+
+        this.samples = new short[batchSize * 2];
+        this.batchSize = batchSize;
+        this.samplesDecoder = new SamplesDecoder(stream, batchSize * 2);
     }
-    public int readBatch(int[] batch) throws IOException{
-        Preconditions.checkArgument(batch.length == batchsize);
-        short[] shortBatch = new short[2*batchsize];
-        int signedBatch = samplesDecoder.readBatch(shortBatch);
+
+    /**
+     * This method reads a batch of samples and computes the power of the samples.
+     *
+     * @param batch the batch of samples
+     * @return the number of power samples written in the batch
+     * @throws IOException if an I/O error occurs
+     */
+    public int readBatch(int[] batch) throws IOException {
+        Preconditions.checkArgument(batch.length == batchSize);
+        int nbrOfSamplesRead = samplesDecoder.readBatch(samples);
+
+        for (int i = 0; i < nbrOfSamplesRead / 2; i += 2) {
+            int lastOfTheLastHeightIndex = i % 8;
+            lastHeightSamples[lastOfTheLastHeightIndex] = samples[i];
+            lastHeightSamples[lastOfTheLastHeightIndex + 1] = samples[i + 1];
+            batch[i / 2] = computePowerSample(lastHeightSamples, lastOfTheLastHeightIndex + 1);
+        }
+        return nbrOfSamplesRead / 2;
+    }
+
+    /**
+     * This method computes the power of the samples.
+     *
+     * @param lhs   the last height radio samples
+     * @param lotli the index of the last of the last height radio sample
+     * @return the power of the samples
+     */
+    private static int computePowerSample(short[] lhs, int lotli) {
+        int InPP = lhs[(lotli - 7 + 8) % 8] - lhs[(lotli - 5 + 8) % 8] + lhs[(lotli - 3 + 8) % 8] - lhs[(lotli - 1 + 8) % 8];
+        int QnPP = lhs[(lotli - 6 + 8) % 8] - lhs[(lotli - 4 + 8) % 8] + lhs[(lotli - 2 + 8) % 8] - lhs[lotli % 8];
+
+        return (int) (Math.pow(InPP, 2) + Math.pow(QnPP, 2));
     }
 }
---
+++ test/ch/epfl/javions/demodulation/PowerComputerTest.java
@@ -0,0 +1,88 @@
+package ch.epfl.javions.demodulation;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PowerComputerTest {
+
+    @Test
+    public void generalTest() throws Exception {
+        // Prepare input stream
+        byte[] inputBytes = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07};
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(inputBytes);
+
+        // Create PowerComputer object and read batch
+        int batchSize = 8;
+        PowerComputer powerComputer = new PowerComputer(inputStream, batchSize);
+        int[] batch = new int[batchSize];
+        int numBatches = powerComputer.readBatch(batch);
+
+        // Verify results
+        assertEquals(2, numBatches);
+        assertEquals(4844548, batch[0]);
+    }
+
+    @Test
+    void constructorWorksWithNullStream() {
+        assertThrows(NullPointerException.class, () -> new PowerComputer(null, 8));
+    }
+
+    @Test
+    void constructorWorksWithBatchSizeEqualsZero() {
+        String url = getClass().getResource("/samples.bin").getFile();
+        try (InputStream stream = new FileInputStream(url)) {
+            assertThrows(IllegalArgumentException.class, () -> new PowerComputer(stream, 0));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void readBatchWorksNotCorrespondingBatchSize() {
+        String url = getClass().getResource("/samples.bin").getFile();
+        try (InputStream stream = new FileInputStream(url)) {
+            PowerComputer powerComputer = new PowerComputer(stream, 1200);
+            assertThrows(IllegalArgumentException.class, () -> powerComputer.readBatch(new int[1201]));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void readBatchReturnsCorrectNumberOfSampleRead() {
+        String url = getClass().getResource("/samples.bin").getFile();
+        for (int batchSize : new int[]{2416, 1208, 400}) {
+            try (InputStream stream = new FileInputStream(url)) {
+                PowerComputer powerComputerWithBigArray = new PowerComputer(stream, batchSize);
+                int[] batch = new int[batchSize];
+                assertEquals(Math.min(1201, batchSize), powerComputerWithBigArray.readBatch(batch));
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Test
+    void readBatchReadsCorrectlyTheFirst10Sample() {
+        String url = getClass().getResource("/samples.bin").getFile();
+        try (InputStream stream = new FileInputStream(url)) {
+            PowerComputer powerComputer = new PowerComputer(stream, 24);
+            int[] batch = new int[24];
+            powerComputer.readBatch(batch);
+            int[] expectedValues = {73, 292, 65, 745, 98, 4226, 12244, 25722, 36818, 23825};
+
+            for (int i = 0; i < expectedValues.length; i++) {
+                assertEquals(expectedValues[i], batch[i]);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
