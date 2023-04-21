88eaf52efcc1e18f40af723b97ccb2406e7c29ad
KevanLam <90704410+KevanLam@users.noreply.github.com>
2023-04-21 16:37:50 +0200

    Check JavaFX

---
+++ src/ch/epfl/sigcheck/CheckJavaFx.java
@@ -0,0 +1,10 @@
+package ch.epfl.sigcheck;
+
+import javafx.scene.paint.Color;
+
+public class CheckJavaFx {
+    public static void main(String[] args) {
+        Color c = Color.RED;
+        System.out.println(c.getRed());
+    }
+}
